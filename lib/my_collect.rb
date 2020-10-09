45b0388a86812865cc57922268a4f79baa6dccbc
iamrivera <a3m3rivera@gmail.com>
2020-10-09 11:55:28 -0700

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+require 'pry'
+
+def my_collect(array)
+   if block_given?
+     collection = []
+     i = 0 
+        while i < array.length 
+            yield(array[i])
+            collection << yield(array[i])
+            i += 1
+        end
+        collection
+    else
+        array
+    end
+end
 
 
